b02710ab19721593ab5611b663c56831533b2357
viswesh <rvisweshwar@gmail.com>
2024-09-03 15:02:34 +0530

    Commited newly

--- adobe.bat
+++ adobe.bat
@@ -1,4 +1,4 @@
-::copy \\192.168.129.22\IT_Share\Windows_Apps_Setups\Reader_en_install.exe c:\temp
-::start C:\temp\Reader_en_install.exe /sAll /rs /q /VERYSILENT /NORESTART
+copy \\192.168.129.22\IT_Share\Windows_Apps_Setups\Reader_en_install.exe c:\temp
+start C:\temp\Reader_en_install.exe /sAll /rs /q /VERYSILENT /NORESTART
 
 pause
